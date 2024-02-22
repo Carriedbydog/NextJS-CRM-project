295a319b33860462ad3a345759b5800efef3e6b7
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2024-02-22 13:14:22 +0200

    feature/intercepting routes

---
+++ app/(admin)/companies/new/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import CompanyForm from '@/app/components/CompanyForm';
+import React from 'react';
+
+export interface pageProps {}
+
+const page = ({}: pageProps) => {
+  return (
+    <div className="py-6 px-10">
+      <CompanyForm onSubmit={console.log} />;
+    </div>
+  );
+};
+
+export default page;
--- app/(admin)/companies/page.tsx
+++ app/(admin)/companies/page.tsx
@@ -1,34 +1,22 @@
-import AddCompanyButton from '@/app/components/AddCompanyButton';
 import CompanyRow from '@/app/components/CompanyRow';
 import CompanyTable from '@/app/components/CompanyTable';
-import Header from '@/app/components/Header';
-import SearchInput from '@/app/components/SearchInput';
 import { Status } from '@/app/components/StatusLabel';
-import Toolbar from '@/app/components/Toolbar';
 import React from 'react';
 
-interface pageProps {}
+export interface PageProps {}
 
-const page = ({}: pageProps) => {
+export default function Page({}: PageProps) {
   return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
-      <CompanyTable>
-        <CompanyRow
-          id={1}
-          category="Technology"
-          company="Google"
-          status={Status.Pending}
-          promotion={true}
-          country="United States"
-          joinedDate="2021-01-01"
-        />
-      </CompanyTable>
-    </>
+    <CompanyTable>
+      <CompanyRow
+        id={1}
+        category="Products"
+        company="Costco"
+        status={Status.Pending}
+        promotion={true}
+        country="USA"
+        joinedDate="02.19.2023"
+      />
+    </CompanyTable>
   );
-};
-
-export default page;
+}
